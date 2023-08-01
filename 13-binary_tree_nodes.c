d0bf81d350f3fb0cc64187307d94b3b4ec45714b
Imukua <ianmukuaa@gmail.com>
2023-08-01 17:55:05 +0300

    betty fix

--- 13-binary_tree_nodes.c
+++ 13-binary_tree_nodes.c
@@ -9,16 +9,16 @@
 
 size_t binary_tree_nodes(const binary_tree_t *tree)
 {
-    size_t nodes = 0;
+	size_t nodes = 0;
 
-    if (tree == NULL)
-        return (0);
+	if (tree == NULL)
+		return (0);
 
-    if (tree->left != NULL || tree->right != NULL)
-        nodes++;
+	if (tree->left != NULL || tree->right != NULL)
+		nodes++;
 
-    nodes += binary_tree_nodes(tree->left);
-    nodes += binary_tree_nodes(tree->right);
+	nodes += binary_tree_nodes(tree->left);
+	nodes += binary_tree_nodes(tree->right);
 
-    return (nodes);
+	return (nodes);
 }
