275184c3bca390312aa50a908e051280521a338e
Imukua <ianmukuaa@gmail.com>
2023-08-01 17:55:17 +0300

    implemented task 14

---
+++ 14-binary_tree_balance.c
@@ -0,0 +1,23 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_balance - measures the balance factor of a binary tree
+ * @tree: pointer to the root node of the tree to measure the balance factor
+ *
+ * Return: balance factor of a binary tree
+ */
+
+int binary_tree_balance(const binary_tree_t *tree)
+{
+	int left_height = 0, right_height = 0;
+
+	if (tree == NULL)
+		return (0);
+
+	if (tree->left != NULL)
+		left_height = 1 + binary_tree_height(tree->left);
+	if (tree->right != NULL)
+		right_height = 1 + binary_tree_height(tree->right);
+
+	return (left_height - right_height);
+}
