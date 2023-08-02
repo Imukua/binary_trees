c6a8ff182bc1ab957b2e4f3f7aed85f52f85a07e
Imukua <ianmukuaa@gmail.com>
2023-08-02 15:49:39 +0300

    fixed flow

--- 18-binary_tree_uncle.c
+++ 18-binary_tree_uncle.c
@@ -12,12 +12,8 @@ binary_tree_t *binary_tree_uncle(binary_tree_t *node)
 	if (node == NULL || node->parent == NULL || node->parent->parent == NULL)
 		return (NULL);
 
-	binary_tree_t *gramps = node->parent->parent;
+	if (node->parent->parent->left == node->parent)
+		return (node->parent->parent->right);
 
-	if (gramps->right == node->parent)
-		return (gramps->left);
-
-	else if (gramps->left == node->parent)
-		return (gramps->right);
-
+	return (node->parent->parent->left);
 }
